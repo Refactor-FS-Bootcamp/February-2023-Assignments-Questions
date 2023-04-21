e2458e897d09d7c3c16c71dfe1bf083bd7a01fa9
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-04-21 14:53:25 +0530

    Add files via upload

---
+++ first-app-6/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Greet = () => {
+    return (
+        <h1>Greet React</h1>
+    )
+}
+
+export const ReactGreet = () => {
+    return (
+        <h1>Greet ReactGreet</h1>
+    )
+}
