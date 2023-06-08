0b495aab5eb972981d4d53fe9ec9850115b3a772
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-06-08 15:23:43 +0530

    Add files via upload

---
+++ next-crash-course/components/ArticleItem.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from '../styles/Article.module.css'
+
+const ArticleItem = ({ article }) => {
+    return (
+        <Link legacyBehavior href={`/article/${article.id}`}>
+            <a className={styles.card}>
+                <h3>{article.title} &rarr;</h3>
+            </a>
+        </Link>
+    )
+}
+export default ArticleItem
